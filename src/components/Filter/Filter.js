91d98f3b720a40cdb16b19858d2e15ca98e7207c
Olena Nesteruk <pavlena81@gmail.com>
2022-10-14 17:53:18 +0300

    add form

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { Label, Input } from './Filter.styled';
+
+
+
+export const Filter = ({ filter, filterContact }) => {
+ 
+    console.log(filterContact);
+
+  return (
+    <Label>
+      Find contacts by name
+      <Input
+        type="text"
+        name="filter"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={filter}
+        onChange={filterContact}
+      />
+    </Label>
+  );
+};
+
+Label.propTypes = {
+    filter: PropTypes.string.isRequired,
+    filterContact: PropTypes.func.isRequired,
+};
