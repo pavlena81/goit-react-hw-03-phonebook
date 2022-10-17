6d9b41175dfa15fd4182953187fd2df424ae0888
Olena Nesteruk <pavlena81@gmail.com>
2022-10-17 23:50:24 +0300

    update app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,7 @@ export class App extends Component {
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}, ],
-    filter: '',
-  
+    filter: '',  
   }
   
   handleChange = e => {
@@ -82,7 +81,7 @@ export class App extends Component {
    }
 
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(_, prevState) {
     console.log(prevState);
     console.log(this.state);
 
